6e85a7dfb82e15c282eb7cf20daa07d0038310fb
Mike V <mvelasco2020@outlook.com>
2025-10-18 20:05:20 -0500

    try forcing image_tag iteration then push to ecr

--- main.tf
+++ main.tf
@@ -217,7 +217,7 @@ resource "aws_codebuild_webhook" "vcert_lambda_webhook" {
     }
     filter {
       type    = "FILE_PATH"
-      pattern = "(Dockerfile|app\\.py|requirements\\.txt|buildspec\\.yml|image_tag\\.tf)"
+      pattern = "(Dockerfile|app\\.py|requirements\\.txt|buildspec\\.yml|terraform\\.tfvars)"
     }
   }
 }
